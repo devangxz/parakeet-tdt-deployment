93751489e44b2e23a248d0448058839f3a9f9f5d
Dayasingh22 <dayasingh003@gmail.com>
2024-09-17 15:21:10 +0530

    fix errors

--- src/components/mui-theme-provider/index.tsx
+++
@@ -1,8 +0,0 @@
-import { ThemeProvider } from "@emotion/react";
-import { ReactNode } from "react";
-
-import { theme } from "../../../mui.theme";
-
-export default function MuiThemeProvider({ children }: { children?: ReactNode }) {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
-}
