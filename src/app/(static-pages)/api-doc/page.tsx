e90aeb178b8d4461e09e894ccdc1f646c0c7359b
Dayasingh22 <dayasingh003@gmail.com>
2024-10-03 10:45:34 +0530

    fix build error

--- src/app/(static-pages)/api-doc/page.tsx
+++ src/app/(static-pages)/api-doc/page.tsx
@@ -1,6 +1,6 @@
-'use client'
-import SwaggerUI from 'swagger-ui-react'
-import 'swagger-ui-react/swagger-ui.css'
+import dynamic from 'next/dynamic'
+
+const SwaggerUI = dynamic(() => import('swagger-ui-react'), { ssr: false })
 
 export default function Page() {
   return <SwaggerUI url='/openapi.yaml' />
