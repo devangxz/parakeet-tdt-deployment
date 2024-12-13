7c5147b1628b008dbf9788fd476aed3e31cf06e4
Dayasingh22 <dayasingh003@gmail.com>
2024-12-13 10:25:47 +0530

    fix build error

--- src/app/cronjobs/reject-timeout-jobs/route.ts
+++ src/app/cronjobs/reject-timeout-jobs/route.ts
@@ -1,109 +1,118 @@
 import { JobStatus, JobType, OrderStatus } from '@prisma/client'
 import { NextResponse } from 'next/server'
 
-import config from '../../../../../config.json'
+import config from '../../../../config.json'
 import logger from '@/lib/logger'
 import prisma from '@/lib/prisma'
 import { getAWSSesInstance } from '@/lib/ses'
 
 export async function POST() {
-    try {
-        const rejectedFiles: string[] = []
-        const assignedFiles = await prisma.jobAssignment.findMany({
-            where: {
-                status: JobStatus.ACCEPTED,
-            },
-            include: {
-                order: {
-                    include: {
-                        File: true,
-                    },
-                },
-                user: true,
-            },
-        })
+  try {
+    const rejectedFiles: string[] = []
+    const assignedFiles = await prisma.jobAssignment.findMany({
+      where: {
+        status: JobStatus.ACCEPTED,
+      },
+      include: {
+        order: {
+          include: {
+            File: true,
+          },
+        },
+        user: true,
+      },
+    })
 
-        const extensionTimeMultiplier = config.extension_time_multiplier
+    const extensionTimeMultiplier = config.extension_time_multiplier
 
-        for (const file of assignedFiles) {
-            if (file.order.File?.duration) {
-                let durationInMs = file.order.File.duration * config.job_timeout_multiplier * 60 * 1000
+    for (const file of assignedFiles) {
+      if (file.order.File?.duration) {
+        let durationInMs =
+          file.order.File.duration * config.job_timeout_multiplier * 60 * 1000
 
-                if (file.extensionRequested) {
-                    durationInMs += file.order.File.duration * extensionTimeMultiplier * 60 * 1000
-                }
-
-                const requiredTime = new Date(Date.now() - durationInMs)
-
-                if (new Date(file.acceptedTs) < requiredTime) {
-                    await prisma.$transaction(async (prisma) => {
-                        await prisma.jobAssignment.update({
-                            where: {
-                                id: file.id,
-                            },
-                            data: {
-                                status: JobStatus.TIMEDOUT,
-                                cancelledTs: new Date(),
-                            },
-                        })
-
-                        let statusRevert: OrderStatus = OrderStatus.TRANSCRIBED
-                        if (file.order.status === OrderStatus.QC_ASSIGNED) {
-                            statusRevert = OrderStatus.TRANSCRIBED
-                        } else if (
-                            file.order.status === OrderStatus.REVIEWER_ASSIGNED ||
-                            file.order.status === OrderStatus.FINALIZER_ASSIGNED
-                        ) {
-                            statusRevert = OrderStatus.FORMATTED
-                        }
-
-                        await prisma.order.update({
-                            where: {
-                                id: file.orderId,
-                            },
-                            data: {
-                                status: statusRevert,
-                                updatedAt: new Date(),
-                            },
-                        })
-                    })
-
-                    rejectedFiles.push(file.order.fileId)
-
-                    const emailData = {
-                        userEmailId: file.user.email,
-                    }
-
-                    const templateData = {
-                        filename: file.order.File.filename,
-                        transcriber_assignment_timeout: (durationInMs / (60 * 60 * 1000)).toFixed(2),
-                    }
-                    const ses = getAWSSesInstance()
-
-                    switch (file.type) {
-                        case JobType.REVIEW:
-                        case JobType.FINALIZE:
-                            await ses.sendMail('REVIEW_JOB_TIMEOUT', emailData, templateData)
-                            break
-                        case JobType.QC:
-                            await ses.sendMail('QC_JOB_TIMEOUT', emailData, templateData)
-                            break
-                    }
-                }
-            }
+        if (file.extensionRequested) {
+          durationInMs +=
+            file.order.File.duration * extensionTimeMultiplier * 60 * 1000
         }
 
-        logger.info(`Timed out files have been rejected: ${rejectedFiles.join(', ')}`)
-        return NextResponse.json({
-            success: true,
-            rejectedFiles,
-        })
+        const requiredTime = new Date(Date.now() - durationInMs)
 
-    } catch (error) {
-        logger.error(`Error rejecting timed out files: ${error}`)
-        return NextResponse.json({
-            success: false,
-            message: 'Failed to check timed out files',
-        }, { status: 500 })
+        if (new Date(file.acceptedTs) < requiredTime) {
+          await prisma.$transaction(async (prisma) => {
+            await prisma.jobAssignment.update({
+              where: {
+                id: file.id,
+              },
+              data: {
+                status: JobStatus.TIMEDOUT,
+                cancelledTs: new Date(),
+              },
+            })
+
+            let statusRevert: OrderStatus = OrderStatus.TRANSCRIBED
+            if (file.order.status === OrderStatus.QC_ASSIGNED) {
+              statusRevert = OrderStatus.TRANSCRIBED
+            } else if (
+              file.order.status === OrderStatus.REVIEWER_ASSIGNED ||
+              file.order.status === OrderStatus.FINALIZER_ASSIGNED
+            ) {
+              statusRevert = OrderStatus.FORMATTED
+            }
+
+            await prisma.order.update({
+              where: {
+                id: file.orderId,
+              },
+              data: {
+                status: statusRevert,
+                updatedAt: new Date(),
+              },
+            })
+          })
+
+          rejectedFiles.push(file.order.fileId)
+
+          const emailData = {
+            userEmailId: file.user.email,
+          }
+
+          const templateData = {
+            filename: file.order.File.filename,
+            transcriber_assignment_timeout: (
+              durationInMs /
+              (60 * 60 * 1000)
+            ).toFixed(2),
+          }
+          const ses = getAWSSesInstance()
+
+          switch (file.type) {
+            case JobType.REVIEW:
+            case JobType.FINALIZE:
+              await ses.sendMail('REVIEW_JOB_TIMEOUT', emailData, templateData)
+              break
+            case JobType.QC:
+              await ses.sendMail('QC_JOB_TIMEOUT', emailData, templateData)
+              break
+          }
+        }
+      }
     }
+
+    logger.info(
+      `Timed out files have been rejected: ${rejectedFiles.join(', ')}`
+    )
+    return NextResponse.json({
+      success: true,
+      rejectedFiles,
+    })
+  } catch (error) {
+    logger.error(`Error rejecting timed out files: ${error}`)
+    return NextResponse.json(
+      {
+        success: false,
+        message: 'Failed to check timed out files',
+      },
+      { status: 500 }
+    )
+  }
 }
