7c2fa9e80d3e9bceff111d19a8405625aa57c3aa
kautilya101 <kautilya101001@gmail.com>
2025-02-28 10:29:29 +0530

    feat: cf-processing using llm added on frontend

--- src/components/editor/ProcessWithLLM.tsx
+++ src/components/editor/ProcessWithLLM.tsx
@@ -1,6 +1,8 @@
-import { OrderDetails } from "@/app/editor/[fileId]/page";
-import { useState } from "react";
 import ReactQuill from "react-quill";
+
+import { Button } from "../ui/button";
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from "../ui/dialog";
+import { OrderDetails } from "@/app/editor/[fileId]/page";
 
 interface ProcessWithLLMProps {
   transcript: string;
@@ -11,17 +13,31 @@ interface ProcessWithLLMProps {
   updateQuill: (quillRef: React.RefObject<ReactQuill> | undefined, content: string) => void;
 }
 
-const ProcessWithLLM = (
+function ProcessWithLLM (
   { transcript, 
     processWithLLMModalOpen, 
     setprocessWithLLMModalOpen,
     orderDetails,
     quillRef,
     updateQuill  
-  }: ProcessWithLLMProps) => {
+  }: ProcessWithLLMProps) {
+  console.log(transcript.length, orderDetails, quillRef, updateQuill)
   return (
     <div className="">
-
+      <Dialog open={processWithLLMModalOpen} onOpenChange={setprocessWithLLMModalOpen}>
+      <DialogContent className="w-96 sm:w-full lg:max-w-4xl xl:max-w-6xl">
+        <DialogHeader>
+          <DialogTitle>Process with LLM</DialogTitle>
+        </DialogHeader>
+        <DialogFooter className="flex gap-2">
+          <>
+            <Button variant="outline" onClick={() => setprocessWithLLMModalOpen(false)}>
+              Cancel
+            </Button>
+          </>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
     </div>
   )
 }
