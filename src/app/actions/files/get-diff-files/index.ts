9d5d6f8b78ea7942cb7fe8a52668378e5a66e227
kautilya101 <kautilya101001@gmail.com>
2025-03-24 10:02:23 +0530

    fix: approvals tab diff generation fixed

--- src/app/actions/files/get-diff-files/index.ts
+++ src/app/actions/files/get-diff-files/index.ts
@@ -1,6 +1,6 @@
 'use server'
 
-import { FileTag } from '@prisma/client'
+import { FileTag, FileVersion } from '@prisma/client'
 
 import logger from '@/lib/logger'
 import prisma from '@/lib/prisma'
@@ -23,22 +23,37 @@ export async function getDiffFilesAction(fileId: string) {
             throw new Error('ASR file not found')
         }
 
-        const qcFileVersion = await prisma.fileVersion.findFirst({
-            where: {
-                fileId,
-                tag: FileTag.QC_DELIVERED,
-            },
-            orderBy: {
-                updatedAt: 'desc',
-            },
+        const omFileVersion = await prisma.fileVersion.findFirst({
+          where: {
+              fileId,
+              tag: FileTag.OM_EDIT,
+          },
+          orderBy: {
+              updatedAt: 'desc',
+          },
         })
-
-        if (!qcFileVersion || !qcFileVersion.s3VersionId) {
-            throw new Error('QC file not found')
+        let qcFileVersion: FileVersion | null = null;
+        if(!omFileVersion || !omFileVersion.s3VersionId) {
+          qcFileVersion = await prisma.fileVersion.findFirst({
+              where: {
+                  fileId,
+                  tag: FileTag.QC_DELIVERED,
+              },
+              orderBy: {
+                  updatedAt: 'desc',
+              },
+          })
+          if (!qcFileVersion || !qcFileVersion.s3VersionId) {
+              throw new Error('QC file not found')
+          }
         }
-
+        const latestFileVersion = omFileVersion || qcFileVersion
+        if(!latestFileVersion || !latestFileVersion.s3VersionId) {
+          throw new Error('No transcriber edited file version not found')
+        }
+        
         const asrFile = (await getFileVersionFromS3(`${fileId}.txt`, asrFileVersion.s3VersionId)).toString()
-        const qcFile = (await getFileVersionFromS3(`${fileId}.txt`, qcFileVersion.s3VersionId)).toString()
+        const qcFile = (await getFileVersionFromS3(`${fileId}.txt`, latestFileVersion.s3VersionId)).toString()
 
         return {
             success: true,
