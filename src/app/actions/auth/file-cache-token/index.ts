a918b8e9991f8e427bc825ebe1c3ca7ac5ffbf9f
Dayasingh22 <dayasingh003@gmail.com>
2025-04-08 07:56:54 +0530

    fix token length

--- src/app/actions/auth/file-cache-token/index.ts
+++ src/app/actions/auth/file-cache-token/index.ts
@@ -8,32 +8,31 @@ import { redis } from '@/lib/redis'
 import { generateUniqueId } from '@/utils/generateUniqueId'
 
 export async function fileCacheTokenAction(sessionData: Session | null = null) {
-    try {
-        const session = sessionData || await getServerSession(authOptions)
-        const user = session?.user
+  try {
+    const session = sessionData || (await getServerSession(authOptions))
+    const user = session?.user
 
-        if (!user) {
-            return {
-                success: false,
-                message: 'User not found'
-            }
-        }
-
-        const token = generateUniqueId()
-
-        await redis.set(token, JSON.stringify(user), 'EX', 180)
-
-        return {
-            success: true,
-            token,
-            message: 'Successfully generated file cache token'
-        }
-
-    } catch (error) {
-        logger.error('Error getting file cache token', error)
-        return {
-            success: false,
-            message: 'Error getting file cache token',
-        }
+    if (!user) {
+      return {
+        success: false,
+        message: 'User not found',
+      }
     }
+
+    const token = generateUniqueId()
+
+    await redis.set(token, JSON.stringify(user), 'EX', 1800)
+
+    return {
+      success: true,
+      token,
+      message: 'Successfully generated file cache token',
+    }
+  } catch (error) {
+    logger.error('Error getting file cache token', error)
+    return {
+      success: false,
+      message: 'Error getting file cache token',
+    }
+  }
 }
