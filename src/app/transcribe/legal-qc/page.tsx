6d01a5b9eb7bf260b3401ea3b01b00d9de010a9d
Dayasingh22 <dayasingh003@gmail.com>
2025-04-17 20:58:44 +0530

    remove aggrement notice

--- src/app/transcribe/legal-qc/page.tsx
+++ src/app/transcribe/legal-qc/page.tsx
@@ -7,7 +7,6 @@ import AssignedFilesPage from '../components/assigned-files'
 import AvailableFilesPage from '../components/available-files'
 import HistoryFilesPage from '../components/history-files'
 import Motd from '@/components/transcriber-motd/review-with-gemini'
-import AgreementNotice from '@/components/transcriber-notice/aggrement'
 import LlmProcessingNotice from '@/components/transcriber-notice/llm-processing'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
@@ -43,7 +42,6 @@ function LegalQCPageContent() {
   return (
     <div className='h-full flex-1 flex-col p-4 md:flex space-y-3'>
       <Motd />
-      <AgreementNotice />
       <LlmProcessingNotice />
 
       <div>
