11071cce6753905512dff568db6557af7423c663
Dayasingh22 <dayasingh003@gmail.com>
2025-05-17 06:42:45 +0530

    fix ASR

--- src/workers/performASR.ts
+++ src/workers/performASR.ts
@@ -487,7 +487,6 @@ export async function performASR(fileId: string): Promise<ASRResult> {
       audio: fileURL,
       punctuate: true,
       speaker_labels: true,
-      speech_threshold: 0.8,
     })
 
     const assemblyAIEndTime = new Date()
